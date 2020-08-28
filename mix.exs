148e8d4cd7e5bd343532c45613292239504bf376
Blake Kostner <git@btkostner.io>
2020-08-28 12:50:05 -0600

    remove alias

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule BottleEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -16,12 +15,6 @@ defmodule BottleEx.MixProject do
   def application do
     [
       extra_applications: [:logger]
-    ]
-  end
-
-  defp aliases do
-    [
-      "protobuf.gen": ["protoc --elixir_out=./lib/bottle/messages -I priv/protos/ priv/protos/*.proto"
     ]
   end
 
