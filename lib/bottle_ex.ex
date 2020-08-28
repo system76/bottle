57f5a9b6aee8472f6ab6b86f62678a8994f23d7e
Sean Callan <sean@system76.com>
2020-08-28 12:29:45 -0600

    New Mix project

---
+++ lib/bottle_ex.ex
@@ -0,0 +1,2 @@
+defmodule BottleEx do
+end
---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule BottleEx.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bottle_ex,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:protobuf, "~> 0.7.1"},
+      # Only for files generated from Google's protos.
+      # Can be ignored if you don't use Google's protos.
+      # Or you can generate the code by yourself.
+      {:google_protos, "~> 0.1"}
+    ]
+  end
+end
