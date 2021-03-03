de52ac25cfbfd4a6938735c7b2b37cbc7255647e
Sean Callan <sean@system76.com>
2021-03-03 11:22:56 -0700

    Deduce message type

--- lib/bottle/bottle.ex
+++ lib/bottle/bottle.ex
@@ -3,9 +3,9 @@ defmodule Bottle do
   A collection of helper functions used by all consuming and producer services
   """
 
-  def publish({type, message}, opts \\ []) do
+  def publish(message, opts \\ []) do
     {exchange, routing_key} = exchange_and_routing_key(message)
-    encoded_message = wrap_and_encode({type, message}, opts)
+    encoded_message = wrap_and_encode(message, routing_key, opts)
 
     with {:ok, channel} <- AMQP.Application.get_channel(:events) do
       AMQP.Basic.publish(channel, exchange, routing_key, encoded_message)
@@ -19,10 +19,16 @@ defmodule Bottle do
     {Enum.join(exchange_parts, "."), routing_key}
   end
 
-  defp wrap_and_encode({type, message}, opts) do
+  defp wrap_and_encode(message, routing_key, opts) do
+    message_type =
+      routing_key
+      |> String.replace(~r/((?<=[a-z])[A-Z])/, "_\\1", global: true)
+      |> String.downcase()
+      |> String.to_atom()
+
     Bottle.Core.V1.Bottle.new(
       request_id: Keyword.get(opts, :request_id, Bottle.RequestId.write(:http)),
-      resource: {type, message},
+      resource: {message_type, message},
       source: Keyword.fetch(opts, :source),
       timestamp: DateTime.to_unix(DateTime.utc_now())
     )
