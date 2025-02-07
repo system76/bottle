31371ff11d92b01bcab0ad8e2366fce28c766dde
Yongjin Chong <zephyranthes03@gmail.com>
2025-02-07 10:08:11 -0700

    Update amqp 3.0 -> 4.0.0

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Bottle.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:amqp, "~> 3.0"},
+      {:amqp, "~> 4.0.0"},
       # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
       {:cowlib, "~> 2.9.0", override: true},
       # Only for files generated from Google's protos.
