a470810a218b697b239bb8f1b7d8ffc22342f6c7
Yongjin Chong <zephyranthes03@gmail.com>
2025-02-07 15:01:45 -0700

    Update bottle version

--- mix.exs
+++ mix.exs
@@ -21,13 +21,12 @@ defmodule Bottle.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:amqp, "~> 4.0.0"},
+      {:amqp, "~> 3.3.2"},
       # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
       {:cowlib, "~> 2.9.0", override: true},
       # Only for files generated from Google's protos.
       # Can be ignored if you don't use Google's protos.
       # Or you can generate the code by yourself.
-      {:ranch, "~> 2.1", override: true},  # ranch 2.1 
       {:google_protos, "~> 0.1"},
       {:grpc, "~> 0.5.0"},
       {:plug_cowboy, "~> 2.0", optional: true},
